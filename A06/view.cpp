93c3cfa0c316aaaa068b7c489e9808bb52a7f7a1
Giuseppe Tortorelli <peppetort@gmail.com>
2022-04-18 11:37:57 +0200

    A06 assignment

---
+++ A06/view.cpp
@@ -0,0 +1,33 @@
+ // Create a look in direction matrix
+ // Pos    -> Position of the camera
+ // Angs.x -> direction (alpha)
+ // Angs.y -> elevation (beta)
+ // Angs.z -> roll (rho)
+glm::mat4 LookInDirMat(glm::vec3 Pos, glm::vec3 Angs) {
+
+    glm::mat4 roll = glm::rotate(glm::mat4(1), glm::radians(Angs.z), glm::vec3(0,0,1));
+    glm::mat4 elevation = glm::rotate(glm::mat4(1), glm::radians(Angs.y), glm::vec3(1,0,0));
+    glm::mat4 direction = glm::rotate(glm::mat4(1), glm::radians(Angs.x), glm::vec3(0,1,0));
+    glm::mat4 translation = glm::translate(glm::mat4(1), Pos);
+    
+    glm::mat4 camera = translation * direction * elevation * roll;
+    glm::mat4 view = glm::inverse(camera);
+
+	return view;
+}
+
+ // Create a look at matrix
+ // Pos    -> Position of the camera (c)
+ // aim    -> Posizion of the target (a)
+ // Roll   -> roll (rho)
+glm::mat4 LookAtMat(glm::vec3 Pos, glm::vec3 aim, float Roll) {
+    glm::mat4 roll = glm::rotate(glm::mat4(1), glm::radians(Roll), glm::vec3(0,0,1));
+    glm::mat4 view = glm::lookAt(Pos, aim, glm::vec3(0,1,0));
+    
+    glm::mat4 out = roll * view;
+	return out;
+}
+
+
+
+
