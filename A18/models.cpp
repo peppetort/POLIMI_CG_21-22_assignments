d6140e16cda81bae9b61994efd6ca66f9f2ed7ce
Giuseppe Tortorelli <peppetort@gmail.com>
2022-05-23 15:05:00 +0200

    spring + sphere

--- A18/models.cpp
+++ A18/models.cpp
@@ -298,81 +298,122 @@ void makeModels() {
 
     // Resizes the vertices array. Repalce the values with the correct number of
     // vertices
-    M3_vertices.resize(3);
+    float xCircle, yCircle, zCircle, xyCircle;
+    float radiusCircle = 2.0f;
+    float stackCount = 48.0f; //24.0f;
+    float sectorCount = 144.0f; //72.0f;
+    float sectorStep = 2 * M_PI / sectorCount;
+    float stackStep = M_PI / stackCount;
+    float sectorAngle, stackAngle;
+    int valueOfArrayCircle = 0;
+    int valueOfSecondArrayCircle = 0;
+    int k1Circle, k2Circle;
+    
+    M3_vertices.resize(stackCount * sectorCount * 2);
+    
+    
+    for(int i=0;i<=stackCount;i++){
+        stackAngle = M_PI / 2 - i * stackStep;
+        xyCircle = radiusCircle * cos(stackAngle);
+        zCircle = radiusCircle * sin(stackAngle);
+        
+        for(int j=0;j<=sectorCount;j++){
+            sectorAngle = j * sectorStep;
+            
+            xCircle = xyCircle * cos(sectorAngle);
+            yCircle = xyCircle * sin(sectorAngle);
 
-    // Vertices definitions
-    M3_vertices[0].pos  = glm::vec3(0.0,1.0,-1.2);
-    M3_vertices[0].norm = glm::vec3(0.0,0.0,1.0);
-    M3_vertices[1].pos  = glm::vec3(-0.866,-0.5,-1.2);
-    M3_vertices[1].norm = glm::vec3(0.0,0.0,1.0);
-    M3_vertices[2].pos  = glm::vec3(0.866,-0.5,-1.2);
-    M3_vertices[2].norm = glm::vec3(0.0,0.0,1.0);
-
-
-    // Resizes the indices array. Repalce the values with the correct number of
-    // indices (3 * number of triangles)
-    M3_indices.resize(3);
-
-    // indices definitions
-    M3_indices[0] = 0;
-    M3_indices[1] = 1;
-    M3_indices[2] = 2;
-
-
-
-
-
-
+            M3_vertices[valueOfArrayCircle].pos = glm::vec3(xCircle, yCircle, zCircle);
+            M3_vertices[valueOfArrayCircle].norm = glm::vec3(xCircle, yCircle, zCircle);
+            valueOfArrayCircle++;
+        }
+    }
+    
+    M3_indices.resize(3 * stackCount * sectorCount * 2);
+    
+    for(int i=0;i<stackCount;i++){
+        k1Circle = i * (sectorCount + 1);
+        k2Circle = k1Circle + sectorCount + 1;
+        
+        for(int j=0;j<sectorCount;j++, k1Circle++, k2Circle++){
+            if(i != 0){
+                M3_indices[valueOfSecondArrayCircle] = k1Circle;
+                valueOfSecondArrayCircle++;
+                M3_indices[valueOfSecondArrayCircle] = k2Circle;
+                valueOfSecondArrayCircle++;
+                M3_indices[valueOfSecondArrayCircle] = k1Circle + 1;
+                valueOfSecondArrayCircle++;
+            }
+            
+            if(i != stackCount -1){
+                M3_indices[valueOfSecondArrayCircle] = k1Circle + 1;
+                valueOfSecondArrayCircle++;
+                M3_indices[valueOfSecondArrayCircle] = k2Circle;
+                valueOfSecondArrayCircle++;
+                M3_indices[valueOfSecondArrayCircle] = k2Circle + 1;
+                valueOfSecondArrayCircle++;
+            }
+        }
+    }
 
 
 
     //// M4 : Spring
     // Replace the code below, that creates a simple octahedron, with the one to create a spring.
-    M4_vertices.resize(6);
-
-    // Vertices definitions
-    M4_vertices[0].pos  = glm::vec3(0.0,1.414,-1.0);
-    M4_vertices[0].norm = glm::vec3(0.0,0.0,1.0);
-    M4_vertices[1].pos  = glm::vec3(0.0,-1.414,-1.0);
-    M4_vertices[1].norm = glm::vec3(0.0,0.0,1.0);
-    M4_vertices[2].pos  = glm::vec3(-1.0,0.0,-2.0);
-    M4_vertices[2].norm = glm::vec3(0.0,0.0,1.0);
-    M4_vertices[3].pos  = glm::vec3(-1.0,0.0,0.0);
-    M4_vertices[3].norm = glm::vec3(0.0,-1.0,0.0);
-    M4_vertices[4].pos  = glm::vec3(1.0,0.0,0.0);
-    M4_vertices[4].norm = glm::vec3(0.0,-1.0,0.0);
-    M4_vertices[5].pos  = glm::vec3(1.0,0.0,-2.0);
-    M4_vertices[5].norm = glm::vec3(0.0,-1.0,0.0);
-
-
-    // Resizes the indices array. Repalce the values with the correct number of
-    // indices (3 * number of triangles)
-    M4_indices.resize(3 * 8);
-
-    // indices definitions
-    M4_indices[0]  = 0;
-    M4_indices[1]  = 2;
-    M4_indices[2]  = 3;
-    M4_indices[3]  = 1;
-    M4_indices[4]  = 3;
-    M4_indices[5]  = 2;
-    M4_indices[6]  = 0;
-    M4_indices[7]  = 3;
-    M4_indices[8]  = 4;
-    M4_indices[9]  = 1;
-    M4_indices[10] = 4;
-    M4_indices[11] = 3;
-    M4_indices[12] = 0;
-    M4_indices[13] = 4;
-    M4_indices[14] = 5;
-    M4_indices[15] = 1;
-    M4_indices[16] = 5;
-    M4_indices[17] = 4;
-    M4_indices[18] = 0;
-    M4_indices[19] = 5;
-    M4_indices[20] = 2;
-    M4_indices[21] = 1;
-    M4_indices[22] = 2;
-    M4_indices[23] = 5;
+    int nSSlices = 36;
+    float steps = 100;
+    float r = 0.5; //cylinder radius
+    float R = 4; //distance from the center of cylinder from center of the spring
+    float t = 0.f;
+    int n = 4; // number of rounds
+    float d = 2; //distance between rounds
+    
+    float sCx = 0, sCy = 0, sCz = 0;
+    
+        
+    
+    M4_vertices.resize(steps * nSSlices);
+    
+    
+    for(int i=0;i<steps;i++){
+        t = (float) i / steps*n * 2.0 * M_PI;
+        sCx = R * cos(t);
+        sCy = (d * t) / M_PI;
+        sCz = R * sin(t);
+        for(int j=0;j<nSSlices;j++){
+            float x = sCx + r * cos((float) j / nSSlices * 2.0 * M_PI) * cos(t);
+            float y = sCy +  r * sin((float) j / nSSlices * 2.0 * M_PI);
+            float z = sCz + r * cos((float) j / nSSlices * 2.0 * M_PI) * sin(t);
+            
+            float nx = cos((float) j / nSSlices * 2.0 * M_PI) * cos(t);
+            float ny = sin((float) j / nSSlices * 2.0 * M_PI);
+            float nz = cos((float) j / nSSlices * 2.0 * M_PI) * sin(t);
+            
+            M4_vertices[j+i*nSSlices].pos = glm::vec3(x,y,z);
+            M4_vertices[j+i*nSSlices].norm = glm::vec3(nx, ny, nz);
+            
+        }
+    }
+    
+    M4_indices.resize(3 * (steps * nSSlices) * 2);
+    
+    int lastVal = 0;
+    
+    for(int i=0;i<steps-1;i++){
+        for(int j=0;j<nSSlices;j++){
+            M4_indices[j*3 + i*nSSlices*3 + 0]= j + i*nSSlices;
+            M4_indices[j*3 + i*nSSlices*3 + 1]= j + i*nSSlices + nSSlices;
+            M4_indices[j*3 + i*nSSlices*3 + 2]= ((j+1) % nSSlices) + nSSlices*i;
+        }
+        lastVal = i*nSSlices*3;
+    }
+    
+    for(int i=0;i<steps-1;i++){
+        for(int j=0;j<nSSlices;j++){
+            M4_indices[j*3 + (steps+i)*nSSlices*3 + 0]= j + i*nSSlices + nSSlices;
+            M4_indices[j*3 + (steps+i)*nSSlices*3 + 1]= ((j+1) % nSSlices) + nSSlices*i + nSSlices;
+            M4_indices[j*3 + (steps+i)*nSSlices*3 + 2]= ((j+1) % nSSlices) + nSSlices*i;
+        }
+    }
 
 }
